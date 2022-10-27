291a6de383787e9aa23a83cc47fb6008dd56011e
bwzou123 <bwzou123@gmail.com>
2022-10-26 21:44:57 -0400

    Create scratch_1.py

---
+++ scratch_1.py
@@ -0,0 +1,16 @@
+
+# a, b, c = map(int,input().split()) => fixed number of inputs
+# list = [int(i) for i in input().split()] => adds elements to a listn
+# def most_frequent(List):  => most frequent num
+#     return max(set(List), key = List.count)
+# counter for total, total = 0 if condition isn't met in subsequence
+# @cache
+
+
+from collections import Counter
+
+
+n = int(input())
+x = int(input())
+
+print(n + x)
